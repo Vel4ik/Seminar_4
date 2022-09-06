8cab07c99b64580c2e537bd9e6abc17f7defbcc2
Valentina <vkpavlenina@mail.ru>
2022-09-06 21:52:26 +0500

    Task_25 project

---
+++ Task_25/Program.cs
@@ -0,0 +1,14 @@
+﻿while (true)
+{
+    Console.WriteLine("Введите next для продолжения или end для выхода");
+    string Operator = Console.ReadLine();
+    if (Operator == "end")
+    {
+        break;
+    }
+    Console.WriteLine("Введите первое число");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(Math.Pow(number1, number2));
+}
